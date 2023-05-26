07cd98f45de4a0323533fb3f5883c7a6f786efbe
Zoe Guidry <oyezoey@gmail.com>
2023-05-26 10:27:37 -0500

    lab completed.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
+    return [num for num in num_list if num % 2 == 0]
     pass
 
 def make_exclamation(sentence_list):
+    return [f'{sentence}!' for sentence in sentence_list]
     pass
